ba0f26e079d6dff4d4ea4f531b866f0f3cbdb1cb
Leo C <8117leo@gmail.com>
2022-04-02 18:45:13 -0400

    initial setup

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import operator
 
-# Create your views here.
+def home(request):
+    return render(request, "word.html", {"conttext":"100"} )
+
+def countword(request):
+    words = request.GET['fulltext']
+    wordlist=words.split(" ")
+    worddict= {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+    sortedword = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+    print(wordlist)
+    print(sortedword)
+
+    return render(request, 'count.html', {"words":words, "len":len(wordlist), "worddict": sortedword })
+
+def about(request):
+    return render(request, "about.html")
