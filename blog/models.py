ab9e0062bb544965b1e45275723dc88b11b85023
Leo C <8117leo@gmail.com>
2022-05-28 18:06:31 -0400

    initial setup

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+    def summary (self):
+        return self.body[:200]
+    def pub_date_pretty(self):
+        return self.pub_date.striftime(" %b %d, %Y ")
